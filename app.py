4d94ebcc9f7b135b6222e99842ddc11f982696b8
DarraghReid <darragh.reid@gmail.com>
2021-06-18 09:38:31 +0000

    implement error pages, add comments

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
-# Pagination activity limit
+# jokes per page
 PER_PAGE = 8
 
 # create instance of Flask in variable 'app'
@@ -36,7 +36,8 @@ mongo = PyMongo(app)
 
 
 # Pagination
-# https://gist.github.com/mozillazg/69fb40067ae6d80386e10e105e6803c9
+# credit to Ed B, whose code I referenced
+# https://github.com/Edb83/self-isolution/blob/master/app.py
 def paginated(jokes):
     page, per_page, offset = get_page_args(
         page_parameter='page', per_page_parameter='per_page')
@@ -55,7 +56,9 @@ def pagination_args(jokes):
 
 # calculates user's age
 def get_age():
+    # default user's age
     user_age = -1
+    # if the user is signed in
     if "user" in session:
         # find user's date of birth from the database
         db_dob = mongo.db.users.find_one(
@@ -80,9 +83,13 @@ def get_age():
             return today.year - born.year - (
                 (today.month, today.day) < (born.month, born.day))
 
+        # assign user's age to user_age, return for use in other views
         user_age = calculate_age(dob)
         return user_age
     else:
+        # if the user is not signed in, user_age is -1
+        # this ensures user will have only restricted access to site
+        # see jokes.html for more information
         user_age == -1
         return user_age
 
@@ -98,6 +105,7 @@ def get_jokes():
     # find all favourites from user_favourites collection in MongoDB
     fav_jokes = list(mongo.db.user_favourites.find())
 
+    # if the user is over 18, all jokes are available to them
     if int(user_age) >= 18:
         # find all jokes from jokes collection in MongoDB
         jokes = list(mongo.db.jokes.find())
@@ -114,6 +122,7 @@ def get_jokes():
             user_age=user_age,
             pagination=pagination,
             )
+        # if the user is under 18, only age appropriate jokes are available
     else:
         # find all age appropriate jokes from jokes collection in MongoDB
         age_app_jokes = list(mongo.db.jokes.find({"for_children": "on"}))
@@ -132,17 +141,17 @@ def get_jokes():
             )
 
 
+# gets all users from the db
 @app.route("/get_users")
 def get_users():
     # find all users from users collection in MongoDB
     users = list(mongo.db.users.find())
-    # print(users_)
 
     # pagination of jokes
     users_paginated = paginated(users)
     pagination = pagination_args(users)
 
-    # render jokes.html template, pass variables into it
+    # render users.html template, pass variables into it
     return render_template(
         "users.html",
         users=users_paginated,
@@ -150,9 +159,11 @@ def get_users():
         )
 
 
+# deletes selected user from db
 @app.route("/delete_user/<user_id>")
 def delete_user(user_id):
     mongo.db.users.remove({"_id": ObjectId(user_id)})
+    # confirmation message
     flash("User removed")
     return redirect(url_for("get_users"))
 
@@ -161,11 +172,13 @@ def delete_user(user_id):
 @app.route("/search", methods=["GET", "POST"])
 # take user_age from get_jokes() view
 def search():
+    # retrieve the search work from the form
     search = request.form.get("search")
 
     # get user's age from get_age()
     user_age = get_age()
 
+    # if the user is over 18, all jokes are available to them
     if int(user_age) >= 18:
         # find all docs from jokes collection in MongoDB
         jokes = list(mongo.db.jokes.find({"$text": {"$search": search}}))
@@ -181,12 +194,14 @@ def search():
             user_age=user_age,
             pagination=pagination
             )
+        # if the user is under 18, only age appropriate jokes are available
     else:
         age_app_jokes = []
 
         # find all docs from jokes collection in MongoDB
         jokes = list(mongo.db.jokes.find({"$text": {"$search": search}}))
 
+        # find jokes that are suitable for children
         for joke in jokes:
             if joke["for_children"] == "on":
                 age_app_jokes.append(joke)
@@ -204,8 +219,10 @@ def search():
             )
 
 
+# signs a user up
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+    # if request is POST, sign user up
     if request.method == "POST":
         # check database for input username
         user = mongo.db.users.find_one(
@@ -223,20 +240,25 @@ def sign_up():
             "date_of_birth": request.form.get("date")
         }
 
-        # insert user into database
+        # insert dictionary into database
         mongo.db.users.insert_one(sign_up)
 
         # put new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
+        # confirmation message
         flash("You're signed up!")
+        # redirect user to their profile
         return redirect(url_for(
             "profile", username=session["user"]))
 
+    # if request is GET, render sign up form
     return render_template("sign_up.html")
 
 
+# signs a user in
 @app.route("/sign_in", methods=["GET", "POST"])
 def sign_in():
+    # if request is POST, sign user in
     if request.method == "POST":
         # check mongodb for input username
         user = mongo.db.users.find_one(
@@ -244,11 +266,13 @@ def sign_in():
 
         # if username exists, check if input password matches hashed password
         if user:
+            # check if input password matches user's password in the database
             if check_password_hash(
                 user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     # if passwords match, put user into session
                     flash("Welcome, {}!".format(request.form.get("username")))
+                    # redirect user to their profile
                     return redirect(url_for(
                         "profile", username=session["user"]))
             # if passwords don't match, inform user and have them try again
@@ -261,15 +285,18 @@ def sign_in():
             flash("Incorrect username and/or password")
             return redirect(url_for("sign_in"))
 
+    # if request is GET, render sign in form
     return render_template("sign_in.html")
 
 
+# displays user's profile, their own jokes, and favourite jokes
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # set username variable equal to username of session user
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
+    # initialise list for user's favourite jokes
     fav_jokes = []
 
     # find all docs from jokes collection in MongoDB
@@ -277,9 +304,12 @@ def profile(username):
 
     # find all jokes that user has favourited
     for joke in jokes:
+        # find the "favouriter" field in each db joke
         favourites = joke["favouriter"]
         for name in favourites:
+            # check to see if user has favourited that joke
             if name == session["user"]:
+                # if so, insert them into fav_jokes list
                 fav_jokes.append(joke)
 
     # pagination of favourite jokes
@@ -296,9 +326,8 @@ def profile(username):
             pagination=pagination
             )
 
-    return redirect(url_for("sign_in"))
 
-
+# allows user to add joke to their favourites
 @app.route("/add_fav/<joke_id>", methods=["GET", "POST"])
 def add_fav(joke_id):
     # search users for joke passed into view
@@ -307,9 +336,9 @@ def add_fav(joke_id):
     # get the array of users who favourited the joke
     favouriter_array = joke["favouriter"]
 
-    # loop through array to check if user has already liked the joke
+    # loop through list to check if user has already liked the joke
     for name in favouriter_array:
-        # if user's name is in the array,
+        # if user's name is in the list,
         # not_favourited is False, otherwise it's True
         # use negative values to avoid duplication
         if name == session["user"]:
@@ -318,28 +347,33 @@ def add_fav(joke_id):
             not_favourited = True
 
     # if the user hasn't favourited the joke, add user's
-    # name to array of users who favourited the joke
+    # name to list of users who favourited the joke
     if not_favourited:
-        # insert user's name into the favouriter_array array
+        # insert user's name into the favouriter_array list
         favouriter_array.append(session['user'])
 
-        # update db with dictionary
+        # update list in db
         mongo.db.jokes.update_one(
             {"_id": ObjectId(joke_id)},
             {"$set": {"favouriter": favouriter_array}})
 
+        # confirmation message
         flash("Joke favourited!")
+        # redirect user to home
         return redirect(url_for("get_jokes"))
 
     # if user has favourited the joke
     else:
         # inform user they have already favou this joke.
         flash("Joke already favourited")
+        # redirect user to home
         return redirect(url_for("get_jokes"))
 
+    # render jokes.html
     return render_template("jokes.html")
 
 
+# allows user to like a joke
 @app.route("/like_joke/<joke_id>", methods=["GET", "POST"])
 def like_joke(joke_id):
     # search users for joke passed into view
@@ -351,12 +385,12 @@ def like_joke(joke_id):
     # increment joke's likes by 1
     new_likes = int(prev_likes) + 1
 
-    # get the array of users who like the joke
+    # get the list of users who like the joke
     liked_by_array = joke["liked_by"]
 
-    # loop through array to check if user has already liked the joke
+    # loop through list to check if user has already liked the joke
     for name in liked_by_array:
-        # if user's name is in the array,
+        # if user's name is in the list,
         # not_liked is False, otherwise it's True
         # use negative values to avoid duplication
         if name == session["user"]:
@@ -364,14 +398,13 @@ def like_joke(joke_id):
         elif name != session["user"]:
             not_liked = True
 
-
     # if the user hasn't liked the joke, add user's
-    # name to array of users who liked the joke
+    # name to list of users who liked the joke
     if not_liked:
-        # insert user's name into the liked_by array
+        # insert user's name into the liked_by list
         liked_by_array.append(session['user'])
 
-        # update array in db
+        # update list in db
         mongo.db.jokes.update_one(
             {"_id": ObjectId(joke_id)},
             {"$set": {"liked_by": liked_by_array}})
@@ -381,32 +414,67 @@ def like_joke(joke_id):
             {"_id": ObjectId(joke_id)},
             {"$set": {"likes": new_likes}})
 
+        # confirmation message
         flash("You've liked this joke!")
+        # redirect user to home
         return redirect(url_for("get_jokes"))
 
     # if user hasn't liked the joke
     else:
         # inform user they have already liked this joke.
         flash("You've already liked this joke")
+        # redirect user to home
         return redirect(url_for("get_jokes"))
 
+    # render jokes.html template
     return render_template("jokes.html")
 
 
+# allows user to remove a jokes from their favourites
 @app.route("/remove_fav/<joke_id>")
 def remove_fav(joke_id):
-    mongo.db.user_favourites.remove({"_id": ObjectId(joke_id)})
-    flash("Removed from favourites")
-    return redirect(url_for(
-            "profile", username=session["user"]))
-
-
-@app.route("/unlike_joke/<joke_id>")
-def unlike_joke(joke_id):
-    # search users for joke passed into view
+    # search jokes for joke passed into view
     joke = mongo.db.jokes.find_one({"_id": ObjectId(joke_id)})
 
+    # get the jokes of users who favourited the joke
+    favouriter_array = joke["favouriter"]
+
+    # loop through list to check if user has already liked the joke
+    for name in favouriter_array:
+        # if user's name is in the list,
+        # not_favourited is False, otherwise it's True
+        # use negative values to avoid duplication
+        if name == session["user"]:
+            not_favourited = False
+        elif name != session["user"]:
+            not_favourited = True
+
+    # if the user hasn't favourited the joke, add user's
+    # name to list of users who favourited the joke
+    if not_favourited:
+        # insert user's name into the favouriter_array list
+        favouriter_array.append(session['user'])
+
+        # update list in db
+        mongo.db.jokes.update_one(
+            {"_id": ObjectId(joke_id)},
+            {"$set": {"favouriter": favouriter_array}})
+
+        # confirmation message
+        flash("Joke favourited!")
+        # redirect user to home
+        return redirect(url_for("get_jokes"))
+
+
+# allows user to unlike joke
+@app.route("/unlike_joke/<joke_id>")
+def unlike_joke(joke_id):
+    # search db for joke passed into view
+    joke = mongo.db.jokes.find_one({"_id": ObjectId(joke_id)})
+
+    # determine the number of likes the joke currently has
     prev_likes = joke["likes"]
+    # decrement joke's likes by 1
     new_likes = prev_likes - 1
 
     # compile dictionary of joke details
@@ -421,20 +489,26 @@ def unlike_joke(joke_id):
 
     # insert dictionary into db
     mongo.db.jokes.update({"_id": ObjectId(joke_id)}, liked_joke)
+    # confirmation message
     flash("You've unliked this joke")
     return redirect(url_for("get_jokes"))
 
 
+# signs user out
 @app.route("/sign_out")
 def sign_out():
-    # remove user from session cookies
+    # confirmation message
     flash("Signed out")
+    # remove user from session cookies
     session.pop("user")
+    # redirect user to sign in form
     return redirect(url_for("sign_in"))
 
 
+# allows user to add a joke to the db
 @app.route("/add_joke", methods=["GET", "POST"])
 def add_joke():
+    # if request is POSt, add joke to db
     if request.method == "POST":
         # set for_children to "on" in db if for_children switch is truthy
         for_children = "on" if request.form.get("for_children") else "off"
@@ -452,18 +526,23 @@ def add_joke():
         }
         # insert dictionary into db
         mongo.db.jokes.insert_one(joke)
+        # confirmation message
         flash("Joke Added!")
+        # redirect user to add joke form
         return redirect(url_for("add_joke"))
 
+    # if request is GET, render the add joke form
     return render_template("add_joke.html")
 
 
+# allows user to edit their jokes
 @app.route("/edit_joke/<joke_id>", methods=["GET", "POST"])
 def edit_joke(joke_id):
 
     # find joke by id key in db
     joke = mongo.db.jokes.find_one({"_id": ObjectId(joke_id)})
 
+    # if request is POST, edit the joke in the bd
     if request.method == "POST":
         # set for_children to "on" in db if for_children switch is truthy
         for_children = "on" if request.form.get("for_children") else "off"
@@ -481,21 +560,41 @@ def edit_joke(joke_id):
         }
         # insert dictionary into db
         mongo.db.jokes.update({"_id": ObjectId(joke_id)}, edited_joke)
+        # confirmation messsage
         flash("Joke Edited!")
+        # redirect user to home
         return redirect(url_for("get_jokes"))
-        # return redirect(url_for("profile(your_jokes"))
 
+    # if request is POST, render edit joke form, with original joke details
     return render_template(
         "edit_joke.html",
         joke=joke
         )
 
 
+# allows user to delete their jokes
 @app.route("/delete_joke/<joke_id>")
 def delete_joke(joke_id):
+    # remove joke from db
     mongo.db.jokes.remove({"_id": ObjectId(joke_id)})
+    # confirmation message
     flash("Joke removed")
+    # redirect user to home
     return redirect(url_for("get_jokes"))
+
+
+# handles 404 error
+@app.errorhandler(404)
+def error_not_found(error):
+    # if a 404 error occuers, render 404.html
+    return render_template("404.html", error=error), 404
+
+
+# handles 500 error
+@app.errorhandler(500)
+def server_error(error):
+    # if a 500 error occuers, render 500.html
+    return render_template("500.html", error=error), 500
 
 
 # tell app how and where to run application
--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,8 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
 /*-----------------------------modals*/
 
-// ensure correct information is displayed on modal
+// ensure correct information is displayed on modals
+// arguments received from onclick function in both profile.html and jokes.html
 function displayModal(title, description, teller) {
 
   // store modal elements in variables
@@ -15,7 +16,7 @@ function displayModal(title, description, teller) {
   modalDesc = document.querySelector(".modal-joke-description");
   postedBy = document.querySelector(".modal-joke-teller");
 
-  // store modal elements in variables
+  // store favourites modal elements in variables
   favmodalTitle = document.querySelector(".fav-modal-joke-title");
   favmodalDesc = document.querySelector(".fav-modal-joke-description");
   favpostedBy = document.querySelector(".fav-modal-joke-teller");
@@ -25,6 +26,7 @@ function displayModal(title, description, teller) {
   modalDesc.innerHTML = description;
   postedBy.innerHTML = `Posted by: ${teller}`;
 
+  // set innerHTML of favourite modal elements to that of favourite card elemets passed into displayModal()
   favmodalTitle.innerHTML = title;
   favmodalDesc.innerHTML = description;
   favpostedBy.innerHTML = `Posted by: ${teller}`;
@@ -50,17 +52,21 @@ function profileToggle(e) {
 
   // ensure that only one heading at a time is displayed
   if (e.target.className == "see-own") {
+    // if "Your Jokes" is showing, "Your Favourites" is hidden
     favsHead.style.display = "none";
     jokesHead.style.display = "block";
 
+    // if "Your Favourites" is showing, "Your Jokes" is hidden
     favs.style.display = "none";
     jokes.style.display = "block";
   }
   // ensure that only the corresponding section of the heading is displayed
   else if (e.target.className == "see-favs") {
+    // if user's favourite jokes are showing, their own jokes are hidden
     jokesHead.style.display = "none";
     favsHead.style.display = "block";
 
+    // if user's own jokes are showing, their favourite jokes are hidden
     jokes.style.display = "none";
     favs.style.display = "block";
   }
